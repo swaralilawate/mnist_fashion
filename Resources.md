aeb45d4f4425c16377431995618245a097e796c3
swaralilawate <lawateswarali02@gmail.com>
2024-02-22 15:19:12 +0530

    modified:   README.md

---
+++ Resources.md
@@ -0,0 +1,5 @@
+# Commands
+* conda info --envs
+# Questions
+
+* Why is normalisation needed , effect of it on image
---
+++ main.py
@@ -0,0 +1,22 @@
+import tensorflow as tf
+import numpy as np
+from keras.datasets import fashion_mnist
+
+#(x_train, y_train), (x_test, y_test) = keras.datasets.fashion_mnist.load_data()
+
+(x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
+assert x_train.shape == (60000, 28, 28)
+assert x_test.shape == (10000, 28, 28)
+assert y_train.shape == (60000,)
+assert y_test.shape == (10000,)
+
+# Scale images to the [0, 1] range
+x_train = x_train.astype("float32") / 255
+x_test = x_test.astype("float32") / 255
+
+# Make sure images have shape (28, 28, 1)
+x_train = np.expand_dims(x_train, -1)
+x_test = np.expand_dims(x_test, -1)
+print("x_train shape:", x_train.shape)
+print(x_train.shape[0], "train samples")
+print(x_test.shape[0], "test samples")
